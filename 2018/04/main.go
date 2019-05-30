d2fa66521244644e3100ee76c375bae5b6f9dcb6
Ben Morrison <ben@gbmor.dev>
2019-05-30 02:18:14 -0400

    totaling sleep and finding sleepiest guard

--- 2018/04/main.go
+++ 2018/04/main.go
@@ -7,18 +7,21 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
-	"time"
 )
 
+var guardList = make(map[int]*guard)
+
 type guard struct {
-	id       int
-	schedule map[time.Time]string
+	id    int
+	times []string
+	sleep int
 }
 
 func newGuard() *guard {
 	return &guard{
-		id:       0,
-		schedule: make(map[time.Time]string),
+		id:    0,
+		times: make([]string, 0),
+		sleep: 0,
 	}
 }
 
@@ -42,37 +45,75 @@ func getTimes() []string {
 	return times
 }
 
-func parseTimes(times []string) map[int]*guard {
-	guards := make(map[int]*guard)
+func parseTimes(times []string) {
 	lastGuard := 0
 
-	for _, e := range times {
+	for i, e := range times {
 		cols := strings.Split(e, " ")
-		timestamp := fmt.Sprintf("%v %v", cols[0], cols[1])
-		thetime, err := time.Parse("2019-04-29 13:00", timestamp)
+		timestamp := cols[1]
+
+		if strings.HasPrefix(cols[3], "#") {
+			if i > 0 {
+				guardList[lastGuard].sleepTotal()
+			}
+			col := cols[3]
+			id, err := strconv.Atoi(col[1:])
+			quickErr(err)
+			lastGuard = id
+			if _, ok := guardList[id]; !ok {
+				guard := newGuard()
+				guard.id = id
+				guardList[id] = guard
+			}
+			continue
+		}
+		guard := guardList[lastGuard]
+		guard.times = append(guard.times, timestamp)
+	}
+}
+
+func (g *guard) sleepTotal() {
+	var minutes []int
+	for _, e := range g.times {
+		split := strings.Split(e, ":")
+		mins := split[1]
+
+		num, err := strconv.Atoi(mins)
 		quickErr(err)
 
-		var guard *guard
-		if strings.HasPrefix(cols[3], "#") {
-			var err error
-			guard = newGuard()
-			col := cols[3]
-			guard.id, err = strconv.Atoi(col[1:])
-			quickErr(err)
-			lastGuard = guard.id
-			guard.schedule[thetime] = "start"
-			continue
-		}
-		guard = guards[lastGuard]
-		if strings.Contains(cols[2], "falls") {
-			guard.schedule[thetime] = "sleep"
-			continue
-		}
-		if strings.Contains(cols[2], "wakes") {
-			guard.schedule[thetime] = "wake"
-		}
-
+		minutes = append(minutes, num)
 	}
 
-	return guards
+	for i, e := range minutes {
+		if i == 1 || i%2 > 0 {
+			dur := e - minutes[i-1]
+			g.sleep += dur - 1
+		}
+	}
+
+	fmt.Printf("%#v\n", g)
+	g.times = []string{}
+	fmt.Printf("%#v\n", g)
+
+}
+
+func findSleepiest(gl map[int]*guard) (int, int) {
+	id := 0
+	total := 0
+
+	for k, v := range gl {
+		if v.sleep > total {
+			total = v.sleep
+			id = k
+		}
+	}
+
+	return id, total
+}
+
+func main() {
+	times := getTimes()
+	parseTimes(times)
+	id, mins := findSleepiest(guardList)
+	fmt.Printf("\nGuard: %v\nMinutes Asleep: %v\nAnswer: %v\n", id, mins, id*mins)
 }
